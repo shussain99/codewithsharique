d27b630731e9cfd5fc8fb92158caf6c3c5f58167
Sharique <work.sharique@gmail.com>
2022-01-30 02:53:30 +0530

    LinkedList

--- First.py
+++ First.py
@@ -1,3 +1,55 @@
-print("hey bhivgjvhg")
-hggvh
-no means no baygit 
+from locale import currency
+from operator import ne
+
+
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class linkedlist:
+    def __init__(self):
+        self.head=None
+    def ghumo(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp=temp.next
+    def modo(self):
+        temp=self.head
+        prev=self.head
+        curr=self.head
+        n_e=self.head
+        while(curr.next):
+            #curr=prev.next
+            n_e=curr.next
+            curr.next=prev
+            prev=curr
+            curr=n_e
+        curr.next=prev
+        self.head=curr
+        temp.next=None
+
+
+
+if __name__=='__main__':
+    llist=linkedlist()
+
+    llist.head= node(1)
+    a=node(2)
+    b=node(3)
+    c=node(4)
+    d=node(5)
+    e=node(6)
+
+    #linking nodes
+
+    llist.head.next=a
+    a.next=b
+    b.next=c
+    c.next=d
+    d.next=e
+    llist.modo()
+    llist.ghumo()
+        
+
