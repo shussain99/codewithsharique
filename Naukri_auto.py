bc231b63a9fadbaa457e467e553dfac6784e3c85
Sharique <work.sharique@gmail.com>
2022-02-04 23:38:02 +0530

    update

--- Naukri_auto.py
+++ Naukri_auto.py
@@ -1,17 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import json
+
+with open("config.json", "r") as cfg:
+    data= json.load(cfg)
+
 #browser exposes an executable file
 #Through Selenium test we will invoke the executable file which will then #invoke actual browser
+
+
 driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
 
 
 driver.get("https://login.naukri.com/nLogin/Login.php")
 time.sleep(3)
 username= driver.find_element_by_id("usernameField")
-username.send_keys("work.sharique@gmail.com")
+username.send_keys(data["username"])
 password= driver.find_element_by_id("passwordField")
-password.send_keys("Shaan@123")
+password.send_keys(data["password"])
 
 time.sleep(3)
 button = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div/div/div/div[2]/div/form/div[2]/div[3]/div/button[1]").click()
